b0a39f1af8729a404a041a7ff2e54e0a4747d71c
Nick Russo <nijaru7@gmail.com>
2026-01-18 08:08:23 -0800

    chore: remove cloud-specific types from embedded
    Removed CompressionTier and StorageTier enums - these were
    LSM-VEC cloud tiering types that don't belong in embedded.
    
    Embedded only supports SQ8 quantization. Cloud tiering with
    RaBitQ will be implemented in the cloud repo when needed.

--- src/lib.rs
+++ src/lib.rs
@@ -102,9 +102,7 @@ pub mod vector;
 // Re-export core types
 pub use compression::{QueryPrep, ScalarParams};
 pub use distance::{cosine_distance, dot_product, l2_distance, l2_distance_squared};
-pub use types::{
-    CompactionStats, CompressionTier, DistanceMetric, OmenDBError, Result, StorageTier, VectorID,
-};
+pub use types::{CompactionStats, DistanceMetric, OmenDBError, Result, VectorID};
 pub use vector::{
     MetadataFilter, SearchResult, ThreadSafeVectorStore, Vector, VectorStore, VectorStoreOptions,
 };
